bc0b5a7fee39a2adf2fdaf0ce483c174a6868a1d
Kevin Jones <kjones@financialforce.com>
2022-02-27 20:47:37 +0000

    Support gulping flows

--- src/commands/apexlink/gulp.ts
+++ src/commands/apexlink/gulp.ts
@@ -15,8 +15,11 @@
 import { SfdxCommand, flags } from '@salesforce/command';
 import { Messages } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
-//import { ClassReader } from '../../gulp/classes';
-//import { LabelReader } from '../../gulp/labels';
+import { ComponentReader } from '../../gulp/components';
+import { FlowReader } from '../../gulp/flows';
+import { PageReader } from '../../gulp/pages';
+import { ClassReader } from '../../gulp/classes';
+import { LabelReader } from '../../gulp/labels';
 import { SObjectReader } from '../../gulp/sobjects';
 import { StubFS } from '../../gulp/stubfs';
 
@@ -45,21 +48,27 @@ export default class Gulp extends SfdxCommand {
 
   public async run(): Promise<AnyJson> {
     const connection = this.org.getConnection();
-    connection.metadata.pollTimeout = 10*60*1000;
-    connection.metadata.pollInterval = 15*1000;
+    connection.metadata.pollTimeout = 10 * 60 * 1000;
+    connection.metadata.pollInterval = 15 * 1000;
 
     const namespaces = (this.flags.namespaces as string[]) || [];
 
     const stubFS = new StubFS(this.project.getPath());
 
-    //const labelsReader = new LabelReader(connection, namespaces, stubFS).run();
-    //const classesReader = new ClassReader(connection, namespaces, stubFS).run();
+    const labelsReader = new LabelReader(connection, namespaces, stubFS).run();
+    const classesReader = new ClassReader(connection, namespaces, stubFS).run();
     const sobjectReader = new SObjectReader(connection, namespaces, stubFS).run();
+    const pageReader = new PageReader(connection, namespaces, stubFS).run();
+    const componentReader = new ComponentReader(connection, namespaces, stubFS).run();
+    const flowReader = new FlowReader(connection, namespaces, stubFS).run();
 
     const results = {
-      //labels: await labelsReader,
-      //classes: await classesReader,
+      labels: await labelsReader,
+      classes: await classesReader,
       sobjects: await sobjectReader,
+      pages: await pageReader,
+      componentReader: await componentReader,
+      flowReader: await flowReader,
     };
     for (const err in results) {
       if (results[err] !== undefined) throw results[err];
