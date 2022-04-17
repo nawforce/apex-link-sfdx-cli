83cec17619b777bbc400cd6f9af27b8a4093435b
Kevin Jones <kjones@financialforce.com>
2022-04-17 16:57:45 +0100

    Add packages command to aid debugging

--- src/commands/apexlink/packages.ts
+++ src/commands/apexlink/packages.ts
@@ -14,52 +14,40 @@
 
 import { SfdxCommand, flags } from '@salesforce/command';
 import { Messages } from '@salesforce/core';
-import { Gulp as GulpRunner, Logger, LoggerStage } from 'apexlink-gulp';
-
-// import { AnyJson } from '@salesforce/ts-types';
+import { Gulp as GulpRunner } from 'apexlink-gulp';
 
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
 
 // Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
 // or any library that is using the messages framework can also be loaded this way.
-const messages = Messages.loadMessages('apexlink', 'gulp');
+const messages = Messages.loadMessages('apexlink', 'packages');
 
-export default class Gulp extends SfdxCommand {
+export default class Packages extends SfdxCommand {
   public static description = messages.getMessage('commandDescription');
 
-  public static examples = ['$ sfdx apexlink:gulp', '$ sfdx apexlink:gulp --debug --namespaces="ns1,ns2"'];
+  public static examples = ['$ sfdx apexlink:packages', '$ sfdx apexlink:packages --debug"'];
 
-  protected static flagsConfig = {
-    namespaces: flags.array({
-      description: 'gulp metadata for these namespaces, unmanaged metadata is gulped by default',
-    }),
-    debug: flags.boolean({ description: 'show progress messages' }),
-  };
+  protected static flagsConfig = {};
 
   protected static requiresUsername = true;
   protected static supportsDevhubUsername = false;
   protected static requiresProject = true;
 
-  public run(): Promise<void> {
-    const logger = new SfdxLogger();
+  public async run(): Promise<void> {
     const connection = this.org.getConnection();
     connection.metadata.pollTimeout = 10 * 60 * 1000;
     connection.metadata.pollInterval = 15 * 1000;
 
     const runner = new GulpRunner();
-    return runner.update(logger, connection, this.project.getPath(), (this.flags.namespaces as string[]) || []);
-  }
-}
+    const packages = await runner.getOrgNamespaces(connection);
 
-class SfdxLogger implements Logger {
-  public debug(message: string): void {
-    console.log(message);
-  }
-  public error(message: string): void {
-    console.log(message);
-  }
-  public complete(stage: LoggerStage): void {
-    console.log(stage);
+    if (this.flags.json) {
+      this.ux.logJson(packages);
+    } else {
+      packages.map((pkg) => {
+        this.ux.log(`${pkg.namespace}: ${pkg.description}`);
+      });
+    }
   }
 }
